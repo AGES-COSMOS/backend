d7dea7ac281b98d449b9eff803d7cc493617deb3
Sofia <sofia.sarotri24@gmail.com>
2024-09-02 21:45:33 -0300

    criei o CRUD do projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AboutUsModule } from './about-us/about-us.module';
+import { ProjectModule } from './projects/projects.module';
 import { PrismaService } from './prisma.service';
 import { PrismaModule } from './prisma.module';
 import { AboutUsController } from './about-us/about-us.controller';
+import { ProjectController } from './projects/projects.controller';
 
 @Module({
-  imports: [AboutUsModule, PrismaModule],
-  controllers: [AppController, AboutUsController],
+  imports: [AboutUsModule, PrismaModule, ProjectModule],
+  controllers: [AppController, AboutUsController, ProjectController],
   providers: [AppService, PrismaService],
 })
 export class AppModule {}
