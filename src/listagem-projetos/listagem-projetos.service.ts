872509f583daff77b28c08baf17945cf67c1da1d
Dalton <daltonbelman@hotmail.com>
2024-09-07 23:45:07 -0300

    adicionado os objetos ao inves dos id de relacionamento do projeto

--- src/listagem-projetos/listagem-projetos.service.ts
+++ src/listagem-projetos/listagem-projetos.service.ts
@@ -6,6 +6,23 @@ export class ListagemProjetosService {
   constructor(private prisma: PrismaService) {}
 
   async getProjects() {
-    return await this.prisma.project.findMany();
+    return await this.prisma.project.findMany({
+      include: {
+        teacher: true, 
+        institution: true, 
+        ProjectKeyword: {
+          include: {
+            keyword: true, 
+          },
+        },
+        ProjectCategory: {
+          include: {
+            category: true, 
+          },
+        },
+        Event: true, 
+        Post: true,  
+      },
+    });
   }
 }
