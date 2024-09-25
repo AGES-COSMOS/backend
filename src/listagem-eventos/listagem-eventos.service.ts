54d16a3645264ea5374ead0741a4568e967c90ef
Dalton <daltonbelman@hotmail.com>
2024-09-25 18:16:21 -0300

    O get Evento retorna agora uma matriz onde cada data tem uma lista de eventos

--- src/listagem-eventos/listagem-eventos.service.ts
+++ src/listagem-eventos/listagem-eventos.service.ts
@@ -6,12 +6,36 @@ export class ListagemEventosService {
   constructor(private prisma: PrismaService) {}
 
   async getEvents(){
-    return await this.prisma.event.findMany({
+    const events = this.prisma.event.findMany({
         include: {
             institution: true,
             EventCategory: true,
             project: true,
         },
+        orderBy: {
+          date : 'asc'
+        }
+        
       });
+
+      const groupedEvents = (await events).reduce((acc, event) => {
+        const eventDate = event.date.toISOString().split('T')[0];
+        
+        if (!acc[eventDate]) {
+          acc[eventDate] = [];
+        }
+        
+        acc[eventDate].push(event);
+        return acc;
+      }, {});
+      
+      const eventMatrix = Object.entries(groupedEvents);
+  
+      return eventMatrix;
+
+
+
+
+      
     }
 }
