c89e98cd0741512743e2ec40ca9107a897b6941d
Joao Pedro Antoniazi Tonello <joao.antoniazi@edu.pucrs.br>
2024-10-01 14:36:02 -0300

    criei os resources para os endpoints de keyword e category

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProjectController } from './projects.controller';
 import { ProjectService } from './projects.service';
 import { PrismaModule } from 'src/prisma.module';
+import { KeywordModule } from './keyword/keyword.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, KeywordModule, CategoryModule],
   controllers: [ProjectController],
   providers: [ProjectService],
   exports: [ProjectService],
